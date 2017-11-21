f3176082aff53de5e0795f42c0d019f6596bd538
Aluno <aluno@SL04TTDESK06.local>
2017-11-21 12:21:35 -0200

    Teste cors

--- js-avancado/cors.js
+++ js-avancado/cors.js
@@ -1,6 +1,38 @@
 "use strict";
+var conts = {
+    base: "http://demo9792543.mockable.io"
+}
 
-function Request($method, $URL) {
+// Classe Usuario
+
+function Usuario($nome, $telefone) {
+    parametros = arguments;
+    this.setNome = setNome;
+    this.getNome = getNome;
+    this.setTelefone = setTelefone;
+    this.getTelefone = setTelefone;
+
+    var nome = arguments[0];
+    var telefone = arguments[1];
+
+    function setNome($nome) {
+        nome = $nome;
+    }
+
+    function getNome() {
+        return nome;
+    }
+
+    function setTelefone($telefone) {
+        telefone = $telefone;
+    }
+
+    function getTelefone() {
+        return telefone;
+    }
+}
+
+function Request($method, $URL, $callBackSuccess) {
     var xhttp;
     if (window.XMLHttpRequest) {
         //code for Modern browsers
@@ -12,7 +44,7 @@ function Request($method, $URL) {
 
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            return this.responseText;
+            $callBackSuccess(this.response);
         }
     };
 
@@ -20,4 +52,11 @@ function Request($method, $URL) {
     xhttp.send();
 }
 
-Request("GET", "http://demo5948899.mockable.io/usuario");
+Request("GET", conts.base + "/usuario", function($retorno){
+    //console.log($retorno);
+    for (var i=0; i<$retorno.length; i++) {
+        usuario = new Usuario($retorno[i].nome, $retorno[i].telefone)
+        //console.log($retorno[i]);
+    } 
+});
+
