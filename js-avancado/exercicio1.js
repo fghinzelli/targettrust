e9a5b4f38c89a0c109b091cfc7b639575a649586
Aluno <aluno@SL04TTDESK06.local>
2017-11-21 08:45:30 -0200

    exercicio1

---
+++ js-avancado/exercicio1.js
@@ -0,0 +1,9 @@
+var carrinho = [];
+var listaProdutos = [
+    {nome:'Cewin 500mg C/ 30 Comprimidos',preco:18.46, quantidade:2},
+    {nome:'Targifor C Efervescente C/ 16 Comprimidos',preco:29.38, quantidade:7},
+    {nome:'Shampoo A Seco Batiste Fresh 150ml',preco:17.58, quantidade:1},
+    {nome:'Inneov Fermete 60 Drágeas',preco:103.92, quantidade:3},
+    {nome:'Leite Aptamil 2 800g',preco:33.90, quantidade:2},
+    {nome:'Lencos Umedecidos Huggies Turma Monica C/96',preco: 12.84, quantidade:4}
+];
