d6348a37786b0f9e7232fe124c65a3739bc8eadf
Max orto <maxrporto@gmail.com>
2018-03-12 09:25:00 -0300

    Alteracao inicial

--- nodejs/exemplos/exemplo1/server.js
+++ nodejs/exemplos/exemplo1/server.js
@@ -6,7 +6,7 @@ const port = 3001;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
-res.end('Ola Que tal Vivente ----');
+res.end('Hello World');
 });
 
 server.listen(port, hostname, () => {
