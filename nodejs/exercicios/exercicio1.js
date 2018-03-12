3a28e667ef35244b3b45d1f547a32c4c21e4e53f
Max orto <maxrporto@gmail.com>
2018-03-12 10:00:05 -0300

    Exercicio 1

---
+++ nodejs/exercicios/exercicio1.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3002;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    
+    //res.setHeader('Content-Type', 'text/json');
+    //res.end('{"nome": "Fernando Ghinzelli", "nascimento": "14/08/1985", "email": "fghinzelli@mprs.mp.br"}');
+
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`<html>
+                <body>
+                    <table>
+                        <tr><td>Nome:</td><td>Fernando Ghinzeli</td></tr>
+                        <tr><td>Data de Nascimento:</td><td>14/08/2018</td></tr>
+                        <tr><td>Email:</td><td>fghinzelli@mprs.mp.br</td></tr>
+                    </table>
+                </body>
+            </html>`)
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Servidor rodando em http://${hostname}:${port}/`);
+});
